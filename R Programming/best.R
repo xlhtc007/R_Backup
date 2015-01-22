8faec18a0eb7d13fada905c1294b9c8d4a3a71c3
Lynnhom Xue <xlh1221it@gmail.com>
2015-01-22 08:58:57 +0800

    complete best.R

--- R Programming/best.R
+++ R Programming/best.R
@@ -1,13 +1,20 @@
 best <- function(state, outcome) {
+    ## Read outcome data
     data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-    if(!any(data$State == state)){
+    ## Check that state and outcome are valid
+    validState <- data$State == state
+    if(!any(validState)){
         stop("invalid state")
     }
-    if(!any(c("heart attack","heart failure","pneumonia") == outcome)){
+    validOutcome <- c("heart attack","heart failure","pneumonia") == outcome
+    if(!any(validOutcome)){
         stop("invalid outcome")
     }
-    ## Read outcome data
-    ## Check that state and outcome are valid
-    ## Return hospital name in that state with lowest 30-day death
-    ## rate
+    ## Return hospital name in that state with lowest 30-day death rate
+    dataSub <- data[validState,]
+    i <- c(11,17,23)[validOutcome]
+    validRate <- dataSub[i] != "Not Available"
+    rsIndex <- dataSub[i] == min(dataSub[i][validRate],na.rm = TRUE)
+    rs <- sort(dataSub["Hospital.Name"][rsIndex])
+    rs[[1]]
 }
