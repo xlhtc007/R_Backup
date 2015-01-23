3202975a10889252be27e763348d0d841344fccf
Lynnhom Xue <xlh1221it@gmail.com>
2015-01-23 12:14:29 +0800

    add a method to rankhospital.R

--- R Programming/rankhospital.R
+++ R Programming/rankhospital.R
@@ -11,21 +11,39 @@ rankhospital <- function(state, outcome, num = "best") {
         stop("invalid outcome")
     }
     ## Return hospital name in that state with the given rank 30-day death rate
-    dataSub <- data[validState,]
+    ##method 1:
+#     dataSub <- data[validState,]
+#     i <- c(11,17,23)[validOutcome]
+#     outcomeValue <- as.numeric(dataSub[i][dataSub[i] != "Not Available"])
+#     hospitalValue <- dataSub["Hospital.Name"][dataSub[i] != "Not Available"]
+    
+#     if(num == "best"){
+#         num <- 1
+#     }else if(num == "worst"){
+#         num <- length(outcomeValue)
+#     }
+#     
+#     if(num < 0 | num > length(outcomeValue)){
+#         return(NA)
+#     }
+#     
+#     value <- hospitalValue[rank(outcomeValue) == num]
+    ##method 2:
     i <- c(11,17,23)[validOutcome]
-    outcomeValue <- as.numeric(dataSub[i][dataSub[i] != "Not Available"])
-    hospitalValue <- dataSub["Hospital.Name"][dataSub[i] != "Not Available"]
+    dataSub <- data[validState,][,c(2,i)]
+    dataSub <- dataSub[dataSub[,2] != "Not Available",]
+    dataSub <- dataSub[order(dataSub[,1]),]
+    dataSub <- dataSub[order(dataSub[,2]),]
     
     if(num == "best"){
         num <- 1
     }else if(num == "worst"){
-        num <- length(outcomeValue)
+        num <- nrow(dataSub)
     }
     
-    if(num < 0 | num > length(outcomeValue)){
+    if(num < 0 | num > nrow(dataSub)){
         return(NA)
     }
-    
-    value <- hospitalValue[rank(outcomeValue) == num]
-    value
+
+    dataSub[num,1]
 }
