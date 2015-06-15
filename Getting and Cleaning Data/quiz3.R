c0f1621530ba762cb00bbdd54b35f455d2a4b802
Lynnhom Xue <xlh1221it@gmail.com>
2015-06-15 18:05:12 +0800

    add some more

--- Getting and Cleaning Data/quiz3.R
+++ Getting and Cleaning Data/quiz3.R
@@ -105,6 +105,86 @@ restData2 = mutate(restData,zipGroups=cut2(zipCode,g=4))
 table(restData2$zipGroups)
 
 
+# Start with reshaping
+library(reshape2)
+head(mtcars)
+# Melting data frames
+mtcars$carname <- rownames(mtcars)
+carMelt <- melt(mtcars,id=c("carname","gear","cyl"),measure.vars=c("mpg","hp"))
+head(carMelt,n=3)
+tail(carMelt,n=3)
+# Casting data frames
+cylData <- dcast(carMelt, cyl ~ variable)
+cylData
+cylData <- dcast(carMelt, cyl ~ variable,mean)
+cylData
+# Averaging values
+head(InsectSprays)
+tapply(InsectSprays$count,InsectSprays$spray,sum)
+# Another way - split
+spIns =  split(InsectSprays$count,InsectSprays$spray)
+spIns
+# Another way - apply
+sprCount = lapply(spIns,sum)
+sprCount
+# Another way - combine
+unlist(sprCount)
+sapply(spIns,sum)
+# Another way - plyr package
+ddply(InsectSprays,.(spray),summarize,sum=sum(count))
+# Creating a new variable
+spraySums <- ddply(InsectSprays,.(spray),summarize,sum=ave(count,FUN=sum))
+dim(spraySums)
+head(spraySums)
+
+# Completed Managing Data Frames with dplyr - Basic Tools
+library(dplyr)
+chicago <- readRDS("chicago.rds")
+dim(chicago)
+head(select(chicago, 1:5))
+names(chicago)[1:3]
+head(select(chicago, city:dptp))
+head(select(chicago, -(city:dptp)))
+chic.f <- filter(chicago, pm25tmean2 > 30)
+head(select(chic.f, 1:3, pm25tmean2), 10)
+chic.f <- filter(chicago, pm25tmean2 > 30 & tmpd > 80)
+head(select(chic.f, 1:3, pm25tmean2, tmpd), 10)
+# arrange
+# Reordering rows of a data frame (while preserving corresponding 
+# order of other columns) is normally a pain to do in R.
+chicago <- arrange(chicago, date)
+head(select(chicago, date, pm25tmean2), 3)
+tail(select(chicago, date, pm25tmean2), 3)
+chicago <- arrange(chicago, desc(date))
+head(select(chicago, date, pm25tmean2), 3)
+tail(select(chicago, date, pm25tmean2), 3)
+# rename
+# Renaming a variable in a data frame in R is surprising hard to do!
+head(chicago[, 1:5], 3)
+chicago <- rename(chicago, dewpoint = dptp,
+                  pm25 = pm25tmean2)
+head(chicago[, 1:5], 3)
+
+chicago <- mutate(chicago,
+                  pm25detrend=pm25-mean(pm25, na.rm=TRUE))
+head(select(chicago, pm25, pm25detrend))
+# group_by
+# Generating summary statistics by stratum
+chicago <- mutate(chicago,
+                  tempcat = factor(1 * (tmpd > 80),
+                                   labels = c("cold", "hot")))
+hotcold <- group_by(chicago, tempcat)
+summarize(hotcold, pm25 = mean(pm25, na.rm = TRUE),
+          o3 = max(o3tmean2),
+          no2 = median(no2tmean2))
+chicago <- mutate(chicago,
+                  year = as.POSIXlt(date)$year + 1900)
+years <- group_by(chicago, year)
+summarize(years, pm25 = mean(pm25, na.rm = TRUE),
+          o3 = max(o3tmean2, na.rm = TRUE),
+          no2 = median(no2tmean2, na.rm = TRUE))
+
+
 
 
 
