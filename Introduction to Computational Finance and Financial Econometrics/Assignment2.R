ba523eb0b25d036566ff375611dd39d4c61b3a03
Lynnhom Xue <xlh1221it@gmail.com>
2015-07-13 21:41:55 +0800

    assignment

---
+++ Introduction to Computational Finance and Financial Econometrics/Assignment2.R
@@ -0,0 +1,32 @@
+# Question 1
+pnorm(0.1,0.05,0.1,lower.tail = FALSE)
+qnorm(0.3085,0.05,0.1,lower.tail = FALSE)
+
+# Question 2
+pnorm(-0.1,0.05,0.1)
+qnorm(0.0668,0.05,0.1)
+
+# Question 3
+1-pnorm(-0.05,0.05,0.1)-pnorm(0.15,0.05,0.1,lower.tail = FALSE)
+
+# Question 4
+qnorm(0.01,0.05,0.1)
+#qnorm(0.01,0.05,0.1,lower.tail = FALSE)
+
+
+# Question 5
+qnorm(0.05,0.05,0.1)
+#qnorm(0.05,0.05,0.1,lower.tail = FALSE)
+
+# Question 6
+qnorm(0.95,0.05,0.1)
+#qnorm(0.95,0.05,0.1,lower.tail = FALSE)
+
+# Question 7
+qnorm(0.99,0.05,0.1)
+#qnorm(0.99,0.05,0.1,lower.tail = FALSE)
+
+# Question 9
+qnorm(0.01,0.04,0.09)*100000
+qnorm(0.05,0.04,0.09)*100000
+
