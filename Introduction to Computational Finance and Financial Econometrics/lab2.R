2082f0aad02d5d1e039cb81c17466f31832ac87d
Lynnhom Xue <xlh1221it@gmail.com>
2015-07-14 12:43:00 +0800

    improvement to assignment3 and lab2

--- Introduction to Computational Finance and Financial Econometrics/lab2.R
+++ Introduction to Computational Finance and Financial Econometrics/lab2.R
@@ -17,5 +17,42 @@ pnorm(0.15,mean = mu_x, sd = sigma_x) - pnorm(-0.05,mean = mu_x, sd = sigma_x)
 # 1%, 5%, 95% and 99% quantile
 qnorm(c(0.01,0.05,0.95,0.99),mean = mu_x, sd = sigma_x)
 
+# Normally distributed monthly returns
+x_vals <- seq(-0.25, 0.35, length.out = 100)
+MSFT <- dnorm(x_vals,mean = 0.05, sd = 0.1)
+SBUX <- dnorm(x_vals,mean = 0.025, sd = 0.05)
 
+# Normal curve for MSFT
+plot(x_vals, MSFT, type = "l", col = "blue", ylab = "Normal curves", ylim = c(0,8))
 
+# Add a normal curve for SBUX
+lines(x_vals, SBUX, col = "red")
+
+# Add a plot legend
+legend("topleft", legend = c("Microsoft", "Starbucks"), col = c("blue", "red"), lty = 1)
+
+# R ~ N(0.04, (0.09)^2) 
+mu_R <- 0.04
+sigma_R <- 0.09
+
+# Initial wealth W0 equals $100,000
+W0 <- 100000
+
+# The 1% value-at-risk
+qnorm(0.01, mean = mu_R, sd = sigma_R)*W0
+
+# The 5% value-at-risk
+qnorm(0.05, mean = mu_R, sd = sigma_R)*W0
+
+# r ~ N(0.04, (0.09)^2) 
+mu_r <- 0.04
+sigma_r <- 0.09
+
+# Initial wealth W0 equals $100,000
+W0 <- 100000
+
+# The 1% value-at-risk
+(exp(qnorm(0.01, mean = mu_r, sd = sigma_r))-1)*W0
+
+# The 5% value-at-risk
+(exp(qnorm(0.05, mean = mu_r, sd = sigma_r))-1)*W0
