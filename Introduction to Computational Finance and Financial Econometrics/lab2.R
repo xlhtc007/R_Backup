2c1edbb38adf34d014438c855129d7bc42e2bea0
Lynnhom Xue <xlh1221it@gmail.com>
2015-07-14 22:32:00 +0800

    complete lab2

--- Introduction to Computational Finance and Financial Econometrics/lab2.R
+++ Introduction to Computational Finance and Financial Econometrics/lab2.R
@@ -56,3 +56,38 @@ W0 <- 100000
 
 # The 5% value-at-risk
 (exp(qnorm(0.05, mean = mu_r, sd = sigma_r))-1)*W0
+
+# Vectors of prices
+PA <- c(38.23,41.29)
+PC <- c(41.11,41.74)
+
+# Simple monthly returns
+RA <- PA[2]/PA[1]-1
+RC <- PC[2]/PC[1]-1
+
+# Continuously compounded returns
+rA <- log(RA+1)
+rC <- log(RC+1)
+
+# Cash dividend per share
+DA <- 0.1
+
+# Simple total return
+RA_total <- (PA[2]+DA)/PA[1]-1
+
+# Dividend yield
+DY <- DA / PA[1]
+
+# Simple annual return
+RA_annual <- (1+RA)^12-1
+    
+# Continuously compounded annual return
+rA_annual <- log(1+RA_annual)
+
+# Portfolio shares
+xA <- 8000/10000
+xC <- 2000/10000
+
+# Simple monthly return
+xA*RA+xC*RC
+
