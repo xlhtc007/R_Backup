2a7ad8df59a647e82d511e216bb94980a855bfe8
Lynnhom Xue <xlh1221it@gmail.com>
2015-07-21 13:02:21 +0800

    improve lab5

--- Introduction to Computational Finance and Financial Econometrics/lab5.R
+++ Introduction to Computational Finance and Financial Econometrics/lab5.R
@@ -1,5 +1,7 @@
 # Load relevant packages
-library(PerformanceAnalytics);library(zoo);library(tseries);
+library(zoo);
+library(tseries);
+library(PerformanceAnalytics);
 
 # Get the monthly adjusted closing price data on VBLTX, FMAGX and SBUX from Yahoo! using the tseries function get.hist.quote(). Set the sample to Jan 1998 through Dec 2009.
 
@@ -8,12 +10,65 @@ VBLTX_prices <- get.hist.quote(instrument="vbltx", start="1998-01-01",end="2009-
 
 FMAGX_prices <- get.hist.quote(instrument="fmagx", start="1998-01-01",end="2009-12-31", quote="AdjClose",provider="yahoo", origin="1970-01-01",compression="m", retclass="zoo", quiet=TRUE)
 
-SBUX_prices <- 
+SBUX_prices <- get.hist.quote(instrument="sbux", start="1998-01-01",end="2009-12-31", quote="AdjClose",provider="yahoo", origin="1970-01-01",compression="m", retclass="zoo", quiet=TRUE)
     
 # Change the class of the time index to yearmon which is appropriate for monthly data
 # index() and as.yearmon() are functions in the zoo package  
 
 index(VBLTX_prices) <- as.yearmon(index(VBLTX_prices))
 index(FMAGX_prices) <- as.yearmon(index(FMAGX_prices))
-
+index(SBUX_prices) <- as.yearmon(index(SBUX_prices))
 # Inspect your data
+start(SBUX_prices)
+end(SBUX_prices)
+
+
+# Create merged price data
+all_prices = merge(VBLTX_prices,FMAGX_prices,SBUX_prices)
+# Rename columns
+colnames(all_prices) <- c("VBLTX", "FMAGX", "SBUX")
+
+# Calculate cc returns as difference in log prices
+all_returns = diff(log(all_prices))
+    
+# Look at the return data
+start(all_returns)
+end(all_returns)
+colnames(all_returns) 
+head(all_returns)
+
+# Plot returns after using the PerformanceAnalytics function chart.TimeSeries().
+# This function creates a slightly nicer looking plot than plot.zoo()
+chart.TimeSeries(all_returns, legend.loc="bottom", main=" ") 
+
+# The previous charts are a bit hard to read. The PerformanceAnalytics function
+# chart.Bar makes it easier to compare the returns of different assets on the 
+# same plot
+chart.Bar(all_returns, legend.loc="bottom", main=" ")
+
+
+# Cumulative return plot - must use simple returns (!) and not cc returns for this
+# Use PerformanceAnalytics function chart.CumReturns()
+simple_returns <- diff(all_prices)/lag(all_prices, k=-1)
+chart.CumReturns(simple_returns, wealth.index = TRUE, legend.loc = "topleft", main = "Future Value of $1 invested")
+
+# Create matrix with returns
+return_matrix <- coredata(all_returns);
+
+# Generate four panel plots
+par(mfrow=c(2,2))
+hist(return_matrix[,"VBLTX"],main="VBLTX monthly returns",
+     xlab="VBLTX", probability=T, col="slateblue1")
+boxplot(return_matrix[,"VBLTX"],outchar=T, main="Boxplot", col="slateblue1")
+plot(density(return_matrix[,"VBLTX"]),type="l", main="Smoothed density",
+     xlab="monthly return", ylab="density estimate", col="slateblue1")
+qqnorm(return_matrix[,"VBLTX"], col="slateblue1")
+qqline(return_matrix[,"VBLTX"])
+par(mfrow=c(1,1))
+
+# Show boxplot of three series on one plot
+boxplot(return_matrix[,"VBLTX"], return_matrix[,"FMAGX"], return_matrix[,"SBUX"],
+        names=colnames(return_matrix), col="slateblue1")
+
+# Do the same thing using the PerformanceAnalytics function chart.Boxplot
+chart.Boxplot(all_returns)
