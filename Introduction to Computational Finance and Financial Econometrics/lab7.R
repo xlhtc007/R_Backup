451e68707f682098b0551b3bf6171b13b9e119e1
Lynnhom Xue <xlh1221it@gmail.com>
2015-07-28 23:17:18 +0800

    complete lab7

--- Introduction to Computational Finance and Financial Econometrics/lab7.R
+++ Introduction to Computational Finance and Financial Econometrics/lab7.R
@@ -117,13 +117,13 @@ points(x=sigma_portfolio_tangency_bill, y=mu_portfolio_tangency_bill, col="blue"
 text(x=tangency_portfolio$sd, y=tangency_portfolio$er, labels="Tangency", pos=2)
 
 # Define the portfolio ratio's
-tangency_weight <- 
-t_bill_weight <- 
+tangency_weight <- 0.3
+t_bill_weight <- 1 - tangency_weight
 
 # Define the portfolio parameters
-mu_portfolio_efficient <- 
+mu_portfolio_efficient <- t_bill_weight*t_bill_rate + tangency_weight*tangency_portfolio$er
 
-sd_portfolio_efficient <- 
+sd_portfolio_efficient <- tangency_weight*tangency_portfolio$sd
 
 # Plot previous exercises
 plot(sigma_portfolio, mu_portfolio, type="b", pch=16, ylim=c(0, max(mu_portfolio)), xlim=c(0, max(sigma_portfolio)), xlab=expression(sigma[p]), ylab=expression(mu[p]),col=c(rep("green", 18), rep("red", 13)))
@@ -133,5 +133,24 @@ text(x=tangency_portfolio$sd, y=tangency_portfolio$er, labels="Tangency", pos=2)
 points(sigma_portfolio_tangency_bill, mu_portfolio_tangency_bill, type="b", col="blue", pch=16)
 
 # Plot Efficient Portfolio with 30% Tangency
-
+points(x=sd_portfolio_efficient, y=mu_portfolio_efficient, col="orange", type="b", pch=16, cex = 2)
 text(x=sd_portfolio_efficient, y=mu_portfolio_efficient, labels="Efficient Portfolio with 30% Tangency", pos=4, cex=0.75)
+
+# Calculate the weight of the tangency portfolio in the portfolio
+tangency_weight  <- sigma_boeing / tangency_portfolio$sd
+    
+# Calculate the portfolio parameters
+mu_portfolio_efficient <- (1 - tangency_weight)*t_bill_rate + tangency_weight*tangency_portfolio$er
+
+sd_portfolio_efficient <- tangency_weight*tangency_portfolio$sd
+
+# Plot previous exercises
+plot(sigma_portfolio, mu_portfolio,bg="NA", type="b", pch=16, ylim=c(0, max(mu_portfolio)), xlim=c(0, max(sigma_portfolio)), xlab=expression(sigma[p]), ylab=expression(mu[p]),col=c(rep("green", 18), rep("red", 13)))
+text(x=sigma_boeing, y=mu_boeing, labels="Boeing", pos=4)
+text(x=sigma_msft, y=mu_msft, labels="MSFT", pos=4)
+text(x=tangency_portfolio$sd, y=tangency_portfolio$er, labels="Tangency", pos=2)
+points(sigma_portfolio_tangency_bill, mu_portfolio_tangency_bill, type="b", col="blue", pch=16)
+
+# Plot Efficient Portfolio with the same risk as Boeing
+points(sd_portfolio_efficient, mu_portfolio_efficient, type="p", col="orange", pch=16, cex=2)
+text(x=sd_portfolio_efficient, y=mu_portfolio_efficient, labels="Efficient Portfolio with same risk as Boeing", pos=2, cex=0.75)
